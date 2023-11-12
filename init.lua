4cec3c677333c9fb73e4374c3d73fbad4b4983fc
adrianbalbs <adrianbalbs@hotmail.com>
2023-11-13 10:04:25 +1100

    Signature help

--- init.lua
+++ init.lua
@@ -470,6 +470,7 @@ local on_attach = function(_, bufnr)
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Help" })
 
   nmap('<leader>gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('<leader>gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
